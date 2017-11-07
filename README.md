67a662050070ce50257bc9e53d8ceba135a3072b
DC <heyyodc-git@yahoo.com>
2017-11-06 17:54:07 -0800

    completed!

--- README.md
+++ README.md
@@ -1,4 +1,28 @@
 # assignment_css_garden_of_good_and_evil
 Create a CSS garden that adapts to your whims.
 
-by Dennis C
+Synopsis
+-----
+Simple but neat website that changes based on a user's configuration choices
+
+Usage
+-----
+From command-line: 
+```
+node index.js
+```
+
+From browser: 
+```
+localhost:3000
+```
+
+Highlights: 
+-----
+- front-end styled with Bootstrap
+- back-end uses node.js, and Express for routing
+- with Handlebars for templating
+- makes use of GET and POST http methods
+- data stored in cookies
+
+#### by Dennis C :deciduous_tree:
--- index.js
+++ index.js
@@ -1,37 +1,64 @@
 "use strict";
-
 const express = require("express");
 const app = express();
 
 const handlebars = require("express-handlebars");
-// const bodyParser = require("body-parser");
-// const cookieParser = require("cookie-parser");
-// const session = require("express-session");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
 
-const PORT = 8080;
+const GOOD_IMAGE = "carried.jpg";
+const EVIL_IMAGE = "alone.jpg";
+const DEFAULT_IMAGE = "blank.gif";
+const GOOD_CHOICE = "good";
+const EVIL_CHOICE = "evil";
+
+const PORT = 3000;
 
 //setup handlebars
 app.set("views", __dirname + "/views");
 app.engine("handlebars", handlebars({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-//setup cookies and sessions
+//setup middleware
+app.use(cookieParser()); // save cookies in req.cookies
 app.use(express.static(__dirname + "/public"));
-// app.use(cookieParser());
-// app.use(session({ secret: "123456", resave: false, saveUninitialized: true }));
-// app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
-	// Cookies that have not been signed
-	// console.log("Cookies: ", req.cookies);
-	// // Cookies that have been signed
-	// console.log("Signed Cookies: ", req.signedCookies);
-	// console.log("=================");
-	// console.log("Session: ", req.session);
-	// res.setHeader("Set-Cookie", ["dc-cookie-1=DCDC", "dc-cookie-2=YoYoYo!"]);
-	// // res.end("hello\n");
-	res.render("results", { test: "test content!!" });
+	//get cookies
+	let cookieChoice = req.cookies.cookieChoice || "";
+	let colorChoice = req.cookies.colorChoice || "";
+	let goodEvilChoice = req.cookies.goodEvilChoice || DEFAULT_IMAGE;
+
+	res.render("results", {
+		cookie: cookieChoice,
+		color: colorChoice,
+		choice: goodEvilChoice
+	});
 });
+
+app.post("/update", (req, res) => {
+	let cookieChoice = req.body.cookieChoice;
+	let colorChoice = req.body.colorChoice;
+	let goodEvilChoice = req.body.goodEvilChoice;
+
+	//set cookies
+	res.cookie("cookieChoice", cookieChoice);
+	res.cookie("colorChoice", colorChoice);
+	res.cookie("goodEvilChoice", _getImage(goodEvilChoice));
+
+	res.redirect("back");
+});
+
+let _getImage = goodEvilChoice => {
+	let image = DEFAULT_IMAGE;
+	if (goodEvilChoice === GOOD_CHOICE) {
+		image = GOOD_IMAGE;
+	} else if (goodEvilChoice === EVIL_CHOICE) {
+		image = EVIL_IMAGE;
+	}
+	return image;
+};
 
 app.listen(PORT, () => {
 	console.log(`Listening on http://localhost:${PORT}!`);
